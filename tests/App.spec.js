004ed8665aafae8d5f7a6866a61af5e478d5d57e
GrégoireMorane <gh.morane@gmail.com>
2019-03-26 09:09:46 +0100

    tests on button and press done

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, Alert } from 'react-native';
 
 import App from '../App';
 
 describe('App', () => {
   it('renders View', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(View));
+    expect(wrapper.find(View)).toHaveLength(1);
   });
 
   describe('within View', () => {
@@ -17,7 +17,13 @@ describe('App', () => {
     });
     it('renders Button with title "Ajouter un article"', () => {
       const wrapper = shallow(<App />);
-      expect(wrapper.find(Button));
+      expect(wrapper.find(Button).props().title).toEqual('Ajouter un article');
+    });
+    it('simulate button press to be a Alert', () => {
+      const wrapper = shallow(<App />);
+      Alert.alert = jest.fn();
+      wrapper.find(Button).simulate('Press');
+      expect(Alert.alert).toHaveBeenCalledWith('Touché');
     });
   });
 });
